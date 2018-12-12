1d5a733305fceb38b0aa5e94b8e3e89a473b0174
Pedro <pedro>
2018-12-12 15:36:06 +1100

    Add Domain and Test project, Test Api host & User Test

---
+++ src/WooliesX.Api/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WooliesX.Domain.Responses;
+
+namespace WooliesX.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        // GET api/user
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            var user = new UserResponse { Name = "name", Token = "token" };
+            return Ok(user);
+        }
+    }
+}
